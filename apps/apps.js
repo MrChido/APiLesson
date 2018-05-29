c8770b3b2c5be000e6921536503d3e4f13a03338
Caleb <savershot685@gmail.com>
2018-05-29 10:25:08 -0500

    turns out the problem was a security issue

--- apps/apps.js
+++ apps/apps.js
@@ -1,8 +1,8 @@
-$(function(){
-$.getJSON('http://www.omdbapi.com/?s=kamen%20rider&r=json&apikey=a47450a2',
+$(document).ready(function(){
+$.getJSON('https://www.omdbapi.com/?s=kamen%20rider&r=json&apikey=a47450a2',
  function(data){
-  console.log(data)
-    var myData= data.Search;
-    console.log(myData);
+  console.log(data);
+   var myData= data.Search;
+  console.log(myData);
   });
 });
