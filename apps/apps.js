6a0f50d5b0c3fe1de971811954274caa3736aa52
Caleb <savershot685@gmail.com>
2018-05-28 21:59:21 -0500

    minor typing errors corrected. However it still does not function

--- apps/apps.js
+++ apps/apps.js
@@ -1,9 +1,8 @@
 $(function(){
-$.getJSON('http://www.omdbapi.com/?s=kamen%20rider&r=json&apikey=a47450a2', function(data){
-  console.log(data){
-    var myData= data.Search[0].Title;
+$.getJSON('http://www.omdbapi.com/?s=kamen%20rider&r=json&apikey=a47450a2',
+ function(data){
+  console.log(data)
+    var myData= data.Search;
     console.log(myData);
   });
-});
-
 });
